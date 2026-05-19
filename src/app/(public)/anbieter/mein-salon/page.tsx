f05439626f0b06c755b4ae0a97e1fe0741033286
YusufFerhatDemir <YusufFerhatDemir@icloud.com>
2026-05-19 22:00:00 +0200

    fix(i18n): force-dynamic damit Locale-Cookie pro Request gelesen wird (anbieter-mein-salon)

--- src/app/(public)/anbieter/mein-salon/page.tsx
+++ src/app/(public)/anbieter/mein-salon/page.tsx
@@ -1,5 +1,7 @@
 'use client'
 
+export const dynamic = 'force-dynamic'
+
 import { BrandLogo } from '@/components/BrandLogo'
 import BottomNav from '@/components/BottomNav'
 import { useRouter } from 'next/navigation'
